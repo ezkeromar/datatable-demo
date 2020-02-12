864bfb787a6fa54fce7671ec9f5e057fa3ef2655
Omar <ezkeromar@gmail.com>
2020-02-12 18:56:44 +0100

    fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularMaterialDatatableModule } from 'angular-material-datatable';
+import { AngularMaterialDatatableModule } from '@thecreatiiives/angular-material-datatable';
 import { ExampleDataTableComponent } from './example-data-table/example-data-table.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
